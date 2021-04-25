4fa4c0464a59bcf49ed773bdf3c104532568d205
elvisreal <ndupuelvis@gmail.com>
2021-04-25 07:22:05 -1100

    new commit

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//import android.widget.Button
+//import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var count = 0
+
+        button_increment.setOnClickListener {
+            count++
+            increment_textview.text = count.toString()
+        }
+
     }
 }
